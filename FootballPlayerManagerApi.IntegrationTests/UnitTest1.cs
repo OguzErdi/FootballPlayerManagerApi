7bd4097db7dcd04a0f55e7eeaaeb0e1a6dbafb69
erdi.erkmen <erdi.erkmen@trendyol.com>
2023-02-05 13:36:36 +0300

    initialize solution

---
+++ FootballPlayerManagerApi.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace FootballPlayerManagerApi.IntegrationTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
